ee6b9f0aa021af7c0ffea1dace96877c52adb3b6
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-12-31 20:24:52 +0000

    Move num trials and num evaluations to instance variable

--- aco_sim.py
+++ aco_sim.py
@@ -16,6 +16,7 @@ class AntColonySimulation:
     def __init__(
         self,
         num_trials: int,
+        num_evaluations_per_trial: int,
         num_locations: int,
         distance_matrix: np.ndarray,
         flow_matrix: np.ndarray,
@@ -23,6 +24,7 @@ class AntColonySimulation:
         evaporation_rate: float,
     ):
         self.num_trials = num_trials
+        self.num_evaluations_per_trial = num_evaluations_per_trial
         self.num_locations = num_locations
         # Each element of the distance matrix represents the distance between
         # two locations, and each element of the flow matrix represents the
@@ -110,10 +112,10 @@ class AntColonySimulation:
 
     def run_aco_evaluation(self) -> float:
         """
-        Run the ant colony optimisation algorithm.
+        Run an ant colony optimisation evaluation iteration.
 
         Returns:
-            The best fitness found after finishing the evaluation.
+            The best fitness found after finishing the evaluation iteration.
         """
         # Randomly distribute small amounts of pheromone between 0 and 1 on the
         # construction graph.
@@ -125,44 +127,40 @@ class AntColonySimulation:
         for path in paths:
             fitness = self.calculate_fitness(path)
             best_fitness = min(best_fitness, fitness)
-
         return best_fitness
 
-    def run_fitness_evaluations(self, num_evaluations: int) -> float:
+    def run_fitness_evaluations(self) -> float:
         """
         Run the ant colony optimisation algorithm for a number of fitness
         evaluations.
-
-        Args:
-            num_evaluations: The number of fitness evaluations to run the
-                             algorithm for.
 
         Returns:
             The best fitness found after finishing all evaluations.
         """
         best_fitness = float("inf")
-        for i in range(num_evaluations):
+        for i in range(self.num_evaluations_per_trial):
             print(f"Iteration {i} - Current best fitness: {best_fitness}")
             best_fitness = min(best_fitness, self.run_aco_evaluation())
-
         return best_fitness
 
-    def run_trials(self, num_evaluations_per_trial: int) -> List[float]:
+    def run_trials(self) -> List[float]:
         """
         Run the ant colony optimisation algorithm for a number of trials.
 
-        Args:
-            num_evaluations_per_trial: The number of fitness evaluations to run
-                                       the algorithm for each trial.
         Returns:
             A list of the best fitnesses found after finishing all trials.
         """
+        print(
+            "Running experiment with "
+            f"m = {self.num_ant_paths}, e = {self.evaporation_rate}..."
+        )
         best_fitnesses = []
         for _ in range(self.num_trials):
-            best_fitnesses.append(
-                self.run_fitness_evaluations(num_evaluations_per_trial)
-            )
-
+            best_fitnesses.append(self.run_fitness_evaluations())
+        print(
+            f"Experiment {index + 1} (m = {self.num_ant_paths}, "
+            f"e = {self.evaporation_rate}): {best_fitnesses}\n"
+        )
         return best_fitnesses
 
 
@@ -183,11 +181,11 @@ def load_data_file(file_name: str) -> Tuple[int, np.ndarray, np.ndarray]:
     matrix_data = np.loadtxt(file_name, skiprows=1, unpack=True, ndmin=2)
     distance_matrix = matrix_data[0:, 0:num_locations]
     flow_matrix = matrix_data[0:, num_locations:]
-
     return (num_locations, distance_matrix, flow_matrix)
 
 
 if __name__ == "__main__":
+    # Set up the experiment configurations.
     start = time.perf_counter()
     locations, distances, flows = load_data_file("data/Uni50a.dat")
     NUM_TRIALS = 5
@@ -200,19 +198,26 @@ if __name__ == "__main__":
         (10, 0.50),
     ]
 
-    for index, config in enumerate(configs):
-        m, e = config
-        print(f"Running experiment with m = {m}, e = {e}...")
+    # Run the experiments.
+    results = []
+    for index, (m, e) in enumerate(configs):
         aco_sim = AntColonySimulation(
             num_trials=NUM_TRIALS,
+            num_evaluations_per_trial=NUM_EVALUATIONS_PER_TRIAL,
             num_locations=locations,
             distance_matrix=distances,
             flow_matrix=flows,
             num_ant_paths=m,
             evaporation_rate=e,
         )
-        fitness_results = aco_sim.run_trials(NUM_EVALUATIONS_PER_TRIAL)
-        print(f"Experiment {index + 1} (m = {m}, e = {e}):" f"{fitness_results}\n")
+        results.append(aco_sim.run_trials())
 
     end = time.perf_counter()
-    print(f"\nTime taken: {end - start} seconds")
+    print(f"\nTime taken: {end - start} seconds\n")
+    # Display the results.
+    for index, (m, e) in enumerate(configs):
+        print(
+            f"Results of experiments with {NUM_TRIALS} trials and "
+            f"{NUM_EVALUATIONS_PER_TRIAL} evaluations per trial:\n"
+            f"   Experiment {index + 1} (m = {m}, e = {e}): {results[index]}\n"
+        )
