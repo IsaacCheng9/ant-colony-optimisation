8b6101ad36a5a4314720f1460e2bf60993f29d8d
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-12-31 20:00:32 +0000

    Change load data from method to function

--- aco_sim.py
+++ aco_sim.py
@@ -30,27 +30,7 @@ class AntColonySimulation:
             self.num_locations,
             self.distance_matrix,
             self.flow_matrix,
-        ) = self.load_data_file("data/Uni50a.dat")
-
-    def load_data_file(self, file_name: str) -> Tuple[int, np.ndarray, np.ndarray]:
-        """
-        Load a data file that contains the number of locations, the distance
-        matrix, and the flow matrix.
-
-        Args:
-            file_name: The name of the file to get the data from.
-
-        Returns:
-            A tuple of the number of locations, and two 2D numpy arrays
-            containing the distance matrix and the flow matrix.
-        """
-        first_line = np.loadtxt(file_name, ndmin=2, usecols=(0))
-        num_locations = int(first_line[0, 0])
-        matrix_data = np.loadtxt(file_name, skiprows=1, unpack=True, ndmin=2)
-        distance_matrix = matrix_data[0:, 0:num_locations]
-        flow_matrix = matrix_data[0:, num_locations:]
-
-        return (num_locations, distance_matrix, flow_matrix)
+        ) = load_data_file("data/Uni50a.dat")
 
     def generate_ant_paths(self, num_ant_paths: int) -> List[List[int]]:
         """
@@ -62,8 +42,10 @@ class AntColonySimulation:
         Returns:
             A list of ant paths that contain numerical locations.
         """
+        # TODO: Implement this with numpy.
         paths = []
 
+        # TODO: Generate paths based on probabilities that can be calculated from the pheromone matrix.
         for _ in range(num_ant_paths):
             path = list(range(self.num_locations))
             random.shuffle(path)
@@ -84,15 +66,15 @@ class AntColonySimulation:
         fitness = 0
         path.append(path[0])
         for i in range(len(path) - 1):
-            # for j in range(len(path) - 1):
+            for j in range(len(path) - 1):
+                fitness += (
+                    self.distance_matrix[path[i], path[j]]
+                    * self.flow_matrix[path[i], path[j]]
+                )
             # fitness += (
-            #     self.distance_matrix[path[i], path[j]]
-            #     * self.flow_matrix[path[i], path[j]]
+            #     self.distance_matrix[path[i], path[i + 1]]
+            #     * self.flow_matrix[path[i], path[i + 1]]
             # )
-            fitness += (
-                self.distance_matrix[path[i], path[i + 1]]
-                * self.flow_matrix[path[i], path[i + 1]]
-            )
 
         return fitness
 
@@ -159,7 +141,8 @@ class AntColonySimulation:
             The best fitness found after finishing all evaluations.
         """
         best_fitness = float("inf")
-        for _ in range(num_evaluations):
+        for i in range(num_evaluations):
+            print(f"Iteration {i} - Current best fitness: {best_fitness}")
             best_fitness = min(best_fitness, self.run_aco_evaluation())
 
         return best_fitness
@@ -181,6 +164,27 @@ class AntColonySimulation:
             )
 
         return best_fitnesses
+
+
+def load_data_file(file_name: str) -> Tuple[int, np.ndarray, np.ndarray]:
+    """
+    Load a data file that contains the number of locations, the distance
+    matrix, and the flow matrix.
+
+    Args:
+        file_name: The name of the file to get the data from.
+
+    Returns:
+        A tuple of the number of locations, and two 2D numpy arrays
+        containing the distance matrix and the flow matrix.
+    """
+    first_line = np.loadtxt(file_name, ndmin=2, usecols=(0))
+    num_locations = int(first_line[0, 0])
+    matrix_data = np.loadtxt(file_name, skiprows=1, unpack=True, ndmin=2)
+    distance_matrix = matrix_data[0:, 0:num_locations]
+    flow_matrix = matrix_data[0:, num_locations:]
+
+    return (num_locations, distance_matrix, flow_matrix)
 
 
 if __name__ == "__main__":
